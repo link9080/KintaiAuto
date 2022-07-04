19762598ae4e423a61ea9fbb7c7f771753ade7d6
link9080 <92527251+link9080@users.noreply.github.com>
2022-07-04 23:58:45 +0900

    更新処理実装

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,6 +20,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium;
 using System.Threading;
+using KintaiAuto.Controllers.util;
 
 namespace KintaiAuto.Controllers
 {
@@ -29,8 +30,8 @@ namespace KintaiAuto.Controllers
         private readonly ILogger<HomeController> _logger;
         const string RECOLU = "[Recolu]";
         const string RAKURAKU = "[RakuRaku]";
+        const string  urlstring = "https://app.recoru.in/ap/";
         LoginsInfo recolu = new LoginsInfo();
-        ChromeDriver chrome = new ChromeDriver();
         LoginsInfo rakuraku = new LoginsInfo();
        
         public HomeController(ILogger<HomeController> logger)
@@ -50,6 +51,70 @@ namespace KintaiAuto.Controllers
         {
 
             Debug.WriteLine(model.Kintais.Count());
+            ChromeDriver chrome = new ChromeDriver();
+            LoginReadText();
+
+            try
+            {
+                var wait = new WebDriverWait(chrome, TimeSpan.FromSeconds(60));
+
+                //tr固定クラス
+                const string TR_CLASS = "1717-";
+                const string KBN_CLASS = "ID-attendKbn-{0} bg-err form_params";
+
+
+                loginRecolu(chrome, wait);
+
+                List<string> str = new List<string>() { "test" };
+                var rakuPtn = new SelectList(str);
+                for (int i = 0;i <  model.Kintais.Count();i++)
+                {
+                    
+                    var _tr = wait.Until(drv => drv.FindElement(By.CssSelector($"[class='{TR_CLASS + model.Kintais[i].Date.ToString("yyyyMMdd")}']")));
+
+                    //開始
+                    if (!string.IsNullOrEmpty(model.Kintais[i].StrTime))
+                    {
+                        //勤務区分 オンサイト固定
+                        var kbn = _tr.FindElement(By.TagName($"select"));
+                        var select = new SelectElement(kbn);
+                        select.SelectByIndex(1);
+                        var start = _tr.FindElement(By.Id($"chartDto.attendanceDtos[{i}].worktimeStart"));
+                        start.SendKeys(model.Kintais[i].StrTime);
+                    }
+
+                    //終了
+                    if (!string.IsNullOrEmpty(model.Kintais[i].EndTime))
+                    {
+                        var end = _tr.FindElement(By.Id($"chartDto.attendanceDtos[{i}].worktimeEnd"));
+                        end.SendKeys(model.Kintais[i].EndTime);
+                    }
+
+                    //休憩
+                    if (!string.IsNullOrEmpty(model.Kintais[i].KyuStrTime) && !string.IsNullOrEmpty(model.Kintais[i].KyuEndTime))
+                    {
+                        breakTimewrite(_tr, model.Kintais[i], chrome);
+                    }
+
+                    ViewData["Kintais[" + i + "].RakuPtn"] = rakuPtn;
+
+                }
+                //更新押下
+                var updbtn = wait.Until(drv => drv.FindElement(By.Id($"UPDATE-BTN")));
+                updbtn.Click();
+                Thread.Sleep(1 * 1000);
+
+                chrome.SwitchTo().Alert().Accept();
+
+                Thread.Sleep(1 * 1000);
+
+                chromeend(chrome);
+            }
+            catch (System.Exception e)
+            {
+                throw e;
+            }
+
             return View("Index",model);
         }
 
@@ -66,50 +131,21 @@ namespace KintaiAuto.Controllers
             return View();
         }
 
-        private async Task<KintaiView>  Main()
+        private async Task<KintaiView>  Main(DateTime? date = null)
         {
-            var urlstring = "https://app.recoru.in/ap/";
             var model = new KintaiView();
             model.Kintais = new List<Kintai>();
-            
+            ChromeDriver chrome = new ChromeDriver();
 
             WebClient wc = new WebClient();
             try
             {
-                //string htmldocs = wc.DownloadString(urlstring);
-                // Console.WriteLine(htmldocs);
-
-                //var config = Configuration.Default.WithDefaultLoader().WithDefaultCookies().WithJs();
-                //var context = BrowsingContext.New(config);
                 var wait = new WebDriverWait(chrome, TimeSpan.FromSeconds(60));
 
                 //tr固定クラス
                 const string TR_CLASS = "1717-";
+                loginRecolu(chrome, wait);
 
-                //Recolu
-                chrome.Url = urlstring;
-
-                // 企業IDを入力
-                var kigyoElement = wait.Until(drv => drv.FindElement(By.XPath("//input[@id='contractId']")));
-                kigyoElement.SendKeys(recolu.Kigyo);
-                //kigyoElement.SendKeys(Keys.Enter);
-
-                // メールアドレスを入力
-                var mailElement = wait.Until(drv => drv.FindElement(By.XPath("//input[@id='authId']")));
-                mailElement.SendKeys(recolu.ID);
-                //kigyoElement.SendKeys(Keys.Enter);
-
-                // passを入力
-                var passElement = wait.Until(drv => drv.FindElement(By.XPath("//input[@id='password']")));
-                passElement.SendKeys(recolu.PASS);
-                passElement.SendKeys(Keys.Enter);
-
-                //勤務表へ移動
-                var editpage = wait.Until(drv => drv.FindElement(By.LinkText("勤務表")));
-
-                editpage.Click();
-
-                Thread.Sleep(1 * 1000);
 
                 //ログイン後システム日の表が表示される日付を取得
                 var Days = wait.Until(drv => drv.FindElements(By.ClassName("item-day")));
@@ -132,7 +168,7 @@ namespace KintaiAuto.Controllers
                         var _tr = wait.Until(drv => drv.FindElement(By.CssSelector($"[class='{TR_CLASS + kintai.Date.ToString("yyyyMMdd")}']")));
 
                         //休憩開始終了をセット
-                        breakTimeRead(_tr, kintai);
+                        breakTimeRead(_tr, kintai,chrome);
 
                         Debug.WriteLine(start[i].GetAttribute("id"));
                         Debug.WriteLine(start[i].FindElement(By.Id($"chartDto.attendanceDtos[{i-1}].worktimeStart")).GetAttribute("id"));
@@ -158,7 +194,7 @@ namespace KintaiAuto.Controllers
                     }
                     
                 }
-                chromeend();
+                chromeend(chrome);
                 return model;
 
             }
@@ -170,7 +206,35 @@ namespace KintaiAuto.Controllers
 
         }
 
-        private void breakTimeRead(IWebElement _tr,Kintai _kintai)
+        private void loginRecolu(ChromeDriver chrome,WebDriverWait wait)
+        {
+            //Recolu
+            chrome.Url = urlstring;
+
+            // 企業IDを入力
+            var kigyoElement = wait.Until(drv => drv.FindElement(By.XPath("//input[@id='contractId']")));
+            kigyoElement.SendKeys(recolu.Kigyo);
+            //kigyoElement.SendKeys(Keys.Enter);
+
+            // メールアドレスを入力
+            var mailElement = wait.Until(drv => drv.FindElement(By.XPath("//input[@id='authId']")));
+            mailElement.SendKeys(recolu.ID);
+            //kigyoElement.SendKeys(Keys.Enter);
+
+            // passを入力
+            var passElement = wait.Until(drv => drv.FindElement(By.XPath("//input[@id='password']")));
+            passElement.SendKeys(recolu.PASS);
+            passElement.SendKeys(Keys.Enter);
+
+            //勤務表へ移動
+            var editpage = wait.Until(drv => drv.FindElement(By.LinkText("勤務表")));
+
+            editpage.Click();
+
+            Thread.Sleep(1 * 1000);
+        }
+
+        private void breakTimeRead(IWebElement _tr,Kintai _kintai, ChromeDriver chrome)
         {
             var img = _tr.FindElement(By.TagName("img"));
             img.Click();
@@ -191,6 +255,38 @@ namespace KintaiAuto.Controllers
 
             var close = chrome.FindElement(By.CssSelector($"[class='common-btn close']"));
             close.Click();
+        }
+
+        private void breakTimewrite(IWebElement _tr, Kintai _kintai, ChromeDriver chrome)
+        {
+            var img = _tr.FindElement(By.TagName("img"));
+            img.Click();
+
+            Thread.Sleep(1 * 1000);
+
+            var kyustr = chrome.FindElement(By.Id("breaktimeDtos[0].breaktimeStart"));
+            kyustr.SendKeys(_kintai.KyuStrTime);
+
+            var kyuend = chrome.FindElement(By.Id("breaktimeDtos[0].breaktimeEnd"));
+            kyuend.SendKeys(_kintai.KyuEndTime);
+
+            //chrome.ExecuteScript("updateBreaktimeEditDialog();");
+            var btn = chrome.FindElements(By.Id("UPDATE-BTN"))[1];
+            btn.Click();
+
+            Thread.Sleep(1 * 1000);
+
+            chrome.SwitchTo().Alert().Accept();
+
+            Thread.Sleep(1 * 1000);
+            if (chrome.FindElements(By.CssSelector($"[class='common-btn close']")).Count() > 0)
+            {
+
+                var close = chrome.FindElement(By.CssSelector($"[class='common-btn close']"));
+
+                close.Click();
+
+            }
         }
 
         private List<LoginsInfo> LoginReadText()
@@ -266,7 +362,7 @@ namespace KintaiAuto.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        private void chromeend()
+        private void chromeend(ChromeDriver chrome)
         {
             chrome.Quit();
             chrome.Dispose();
