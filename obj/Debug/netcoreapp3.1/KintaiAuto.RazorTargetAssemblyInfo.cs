445b87f5dfcd72fa4bd8a9ac4d66bf010145f066
link9080 <oldmetalgear@gmail.com>
2024-07-22 23:27:53 +0900

    ターゲットフレームワーク6.0に変更

--- obj/Debug/netcoreapp3.1/KintaiAuto.RazorTargetAssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/KintaiAuto.RazorTargetAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyProductAttribute("KintaiAuto")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58ea225c9d0fd7198e00433076102b5bae811e45")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KintaiAuto.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
